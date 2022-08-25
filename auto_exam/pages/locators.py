1103c6ba68a8d96db291a48a7ef97d4e15ad5b47
Alexandr Evdokimov <Alexandr.Evdokimov@icl-services.com>
2022-08-25 18:11:26 +0300

    added login_page file

--- auto_exam/pages/locators.py
+++ auto_exam/pages/locators.py
@@ -1,2 +1,7 @@
 class MainPageLocators:
     LOGIN_LINK = '//a[@id="login_link"]'
+
+
+class LoginPageLocators:
+    LOGIN_FORM = '//form[@id="login_form"]'
+    REGISTER_FORM = '//form[@id="register_form"]'
