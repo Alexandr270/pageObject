f7a6e491774bd452e521baf1b1166c28e2f332c9
Alexandr Evdokimov <Alexandr.Evdokimov@icl-services.com>
2022-08-25 18:41:10 +0300

    comment allert in main_page

--- auto_exam/pages/main_page.py
+++ auto_exam/pages/main_page.py
@@ -1,13 +1,14 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-
+import time
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element_by_xpath(MainPageLocators.LOGIN_LINK)
         login_link.click()
-        alert = self.browser.switch_to.alert
-        alert.accept()
+        # time.sleep(3)
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(MainPageLocators.LOGIN_LINK), "Login link is not presented"
